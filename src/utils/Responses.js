f099cca78ed37df2f2183cbe43089ba16742f8f5
Okocha Ebube <okochaebube@gmail.com>
2020-03-24 22:55:58 +0100

    Update Responses

--- src/utils/Responses.js
+++ src/utils/Responses.js
@@ -5,5 +5,5 @@ module.exports = {
     errorMessage: 'Something went wrong',
     jwtSecretKey: 'norahprivateloginkey',
     parameterCheckGeneralResponse: 'Please send all required params in the body',
-    mongoDbUrl: 'mongodb+srv://ebube:ebube12345@cluster0-lc1px.mongodb.net/norah?retryWrites=true&w=majority'
+   
 };
